5195439d026b4fedcc2c918f742186b8afc693bf
Marcelo Cavalca Filho <marcelo.cavalca@cptec.inpe.br>
2016-11-09 10:44:14 -0200

    Modo debug
    Tentativa do modo debug para testes

--- _js/items.js
+++ _js/items.js
@@ -1,13 +1,29 @@
 // JavaScript Document
 
 var speed = 0;
-var qtItems = 2;
+var items = 0;
 
-function updateTick(){
-	var valor = document.getElementById('coin-qt').innerHTML;	
-	document.getElementById('coin-qt').innerHTML = parseInt(valor)+qtItems;
+function updateSpeed(){
+	speed = parseInt(document.getElementById('speed-value').value);	
+	document.getElementById('speed-value-r').innerHTML = speed;
+	updateTick();
 }
 
-if (speed !== 0){
-	setInterval(updateTick, speed);
+function updateItems(){
+	items = parseInt(document.getElementById('items-value').value);	
+	document.getElementById('speed-items-r').innerHTML = items;
+	updateTick();
+}
+
+function updateCount(){
+	var valor = document.getElementById('coin-qt').innerHTML;	
+	document.getElementById('coin-qt').innerHTML = parseInt(valor)+items;
+}
+
+function updateTick(){
+	if(speed = 0){
+		speed = 9999;
+	} else {
+	setInterval(updateCount, speed);	
+	}
 }
